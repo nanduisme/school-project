939c0493547f6a0fb7e7265cbf11bcbdc6795804
Nandu M <nandagopalnemnon@gmail.com>
2022-11-03 21:40:02 +0530

    Table maker made into a function, ready for use in main.py

--- table.py
+++ table.py
@@ -1,36 +1,54 @@
-sample_table = [[i, chr(ord("a") + i - 1) * (i + 20), i / 2, i * 2] for i in range(1, 20)]
-attrs = ["ID", "Name", "Float", "FLOATTWICE"]
-CARDINALITY = len(sample_table)
-DEGREE = len(attrs)
-CHAR_LIM = 30
+from random import choice, randint
 
-columns = list(zip(*sample_table[::-1]))
-max_lengths = [
-    (attrs[n], max(map(lambda x: len(str(x)), col[:]))) for n, col in enumerate(columns)
+
+def gen_table(table: list, attrs: list, char_lim=100):
+    CARDINALITY = len(table)
+
+    # Rotates the table
+    columns = list(zip(*table[::-1]))
+
+    # Calculates the max lengths each column has values for
+    max_lengths = {
+        attrs[n]: max(map(lambda x: len(str(x)), col[:] + (attrs[n],)))
+        for n, col in enumerate(columns)
+    }
+
+    ROOF_STR = ""
+    SEP_STR = ""
+    FLOOR_STR = ""
+
+    attr_lengths = []
+    for attr in max_lengths:
+        attr_lengths.append(min(max_lengths[attr], char_lim))
+        ROOF_STR += f"┬{'─' * (attr_lengths[-1] + 2)}"
+        SEP_STR += f"┼{'─' * (attr_lengths[-1] + 2)}"
+        FLOOR_STR += f"┴{'─' * (attr_lengths[-1] + 2)}"
+
+    ROOF_STR = f"╭{ROOF_STR[1:]}╮"
+    SEP_STR = f"├{SEP_STR[1:]}┤"
+    FLOOR_STR = f"╰{FLOOR_STR[1:]}╯"
+
+    body = [ROOF_STR]
+
+    for rcount, row in enumerate([attrs, *table]):
+        rowstr = ""
+        for ccount, col in enumerate(row):
+            col = str(col)
+            if len(col) > char_lim:
+                col = f"{col[:char_lim - 1]}…"
+            rowstr += f"│ {col}{' ' * (attr_lengths[ccount] - len(col))} "
+        body.append(f"{rowstr}│")
+        body.append(SEP_STR if rcount < CARDINALITY else FLOOR_STR)
+
+    return body
+
+
+sample_table = [
+    (
+        randint(1000, 9999),
+        "Nandagopal Menon",
+        randint(1, 12),
+        choice(["A", "B", "C", "D"]),
+    )
+    for _ in range(10)
 ]
-
-roofstr = ""
-sepstr = ""
-floorstr = ""
-
-attr_lengths = []
-for attr, l in max_lengths:
-    attr_lengths.append(min(max(len(attr), l), CHAR_LIM))
-    roofstr += f"┬{'─' * (attr_lengths[-1] + 2)}"
-    sepstr += f"┼{'─' * (attr_lengths[-1] + 2)}"
-    floorstr += f"┴{'─' * (attr_lengths[-1] + 2)}"
-
-roofstr = f"╭{roofstr[1:]}╮\n"
-sepstr = f"├{sepstr[1:]}┤\n"
-floorstr = f"╰{floorstr[1:]}╯\n"
-
-for rcount, row in enumerate([attrs, *sample_table]):
-    for n, col in enumerate(row):
-        col = str(col)
-        if len(col) > CHAR_LIM:
-            col = f"{col[:CHAR_LIM - 1]}…"
-        roofstr += f"│ {col}{' ' * (attr_lengths[n] - len(col))} "
-    roofstr += "│\n"
-    roofstr += sepstr if rcount < CARDINALITY else floorstr
-
-print(roofstr)
