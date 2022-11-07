6ad0f0bad18dc8a37530ff3bca7a92fabaaac669
Nandu M <nandagopalnemnon@gmail.com>
2022-11-07 21:38:20 +0530

    Added rich support to startup

--- startup.py
+++ startup.py
@@ -1,26 +1,27 @@
 import pickle
+from time import sleep
+
+from rich.console import Console
+from rich.prompt import Prompt, IntPrompt, FloatPrompt
+
+console = Console()
 
 
-def get_input(prompt, type_):
-    while True:
-        try:
-            return type_(input(prompt))
-        except ValueError:
-            print("The data you have entered is not of the right format!")
-
-
-def get_input_if_currupt(value, prompt, type_):
+def get_input_if_currupt(value, type_, prompt, prompt_type):
     if isinstance(value, type_):
         return value
-    print("Existing data is not of the right format, please enter it again.")
-    return get_input(prompt)
+    console.print(
+        "Existing data is not of the right format, please enter it again.",
+        style="red bold",
+    )
+    return prompt_type.ask(prompt)
 
 
-USERNAME_PROMPT = "MySQL user name: "
-PASSWORD_PROMPT = "MySQL password: "
-MAX_BOOKS_PROMPT = "Number of books that can be borrowed at a time: "
-MAX_DAYS_PROMPT = "Number of days to keep book (without fine): "
-FINE_PER_DAY_PROMPT = "Fine per day after the due date: "
+USERNAME_PROMPT = "[blue bold]MySQL user name[/]"
+PASSWORD_PROMPT = "[blue bold]MySQL password[/]"
+MAX_BOOKS_PROMPT = "[blue bold]Number of books that can be borrowed at a time[/]"
+MAX_DAYS_PROMPT = "[blue bold]Number of days to keep book (without fine)[/]"
+FINE_PER_DAY_PROMPT = "[blue bold]Fine per day after the due date[/]"
 
 
 def startup():
@@ -34,19 +35,20 @@ def startup():
     try:
         data = pickle.load(f)
     except EOFError:
-        username = get_input(USERNAME_PROMPT, str)
-        password = get_input(PASSWORD_PROMPT, str)
-        max_books = get_input(MAX_BOOKS_PROMPT, int)
-        max_days = get_input(MAX_DAYS_PROMPT, int)
-        fine_per_day = get_input(FINE_PER_DAY_PROMPT, int)
+        username = Prompt.ask(USERNAME_PROMPT, default="root")
+        password = Prompt.ask(PASSWORD_PROMPT, password=True)
+        max_books = IntPrompt.ask(MAX_BOOKS_PROMPT)
+        max_days = IntPrompt.ask(MAX_DAYS_PROMPT)
+        fine_per_day = FloatPrompt.ask(FINE_PER_DAY_PROMPT)
 
         pickle.dump([username, password, max_books, max_days, fine_per_day], f)
-        print("Opening program...")
+        console.print("Opening program...", style="green")
+        sleep(1)
 
         return
 
     # Checks if file data is valid else gets new values and dumps to file
-    username = get_input_if_currupt(data[0], USERNAME_PROMPT, str)
+    username = get_input_if_currupt(data[0], str, USERNAME_PROMPT, Prompt)
     password = get_input_if_currupt(data[1], PASSWORD_PROMPT, str)
     max_books = get_input_if_currupt(data[2], MAX_BOOKS_PROMPT, int)
     max_days = get_input_if_currupt(data[3], MAX_DAYS_PROMPT, int)
