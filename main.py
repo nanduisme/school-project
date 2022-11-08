236136546e5570cb04e307c79fe51f11e2561551
Nandu M <nandagopalnemnon@gmail.com>
2022-11-08 07:19:56 +0530

    checkpoint

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from time import sleep
 from mysql.connector import connect
 from rich.console import Console
 from rich.prompt import Prompt
+from rich.theme import Theme
 
 from db import DatabaseManager
 from startup import get_config, startup
@@ -11,7 +12,18 @@ from startup import get_config, startup
 
 class App:
     def __init__(self) -> None:
-        self.console = Console()
+        self.THEME = Theme(
+            {
+                "header": "green b u",
+                "choice": "cyan b",
+                "error": "red b",
+                "openquit": "green b",
+                "option": "magenta",
+                "input": "yellow",
+            }
+        )
+
+        self.console = Console(theme=self.THEME)
         self.path = ["Home"]
 
         self.config = get_config()
@@ -29,6 +41,10 @@ class App:
     def clear(self):
         os.system("cls")
 
+    def header(self):
+        self.console.print("[header]LIBRARY MANAGEMENT SYSTEM[/]\n\n")
+        self.console.print(f"{self.get_path_str()}\n\n")
+
     def get_path_str(self):
         s = "[blue][b]Home[/]"
         for p in self.path[1:]:
@@ -40,14 +56,13 @@ class App:
         err = ""
         while True:
             self.clear()
-            self.console.print("[green b u]LIBRARY MANAGEMENT SYSTEM[/]\n\n")
-            self.console.print(f"{self.get_path_str()}\n\n")
+            self.header()
 
             for n, option in enumerate(options, 1):
-                self.console.print(f"[magenta u]{n}[/]. {option}")
+                self.console.print(f"[option][u]{n}[/]. {option}")
 
             choice = Prompt.ask(
-                f"\n\n[red b]{err}[/]\n[cyan b]Enter a choice [1-{len(options)}]"
+                f"\n\n[error]{err}[/]\n[choice]Enter a choice [1-{len(options)}][/]"
             )
 
             if choice not in [str(x) for x in range(1, len(options) + 1)]:
@@ -72,7 +87,7 @@ class App:
 
             if choice == 8:
                 self.clear()
-                self.console.print("[green b]Closing app...")
+                self.console.print("[openquit]Closing app...[/]")
                 sleep(1)
                 break
 
@@ -91,6 +106,27 @@ class App:
                 break
 
             self.path.append(OPTIONS[choice - 1].replace(" ...", ""))
+
+            if choice == 1:
+                self.clear()
+                self.header()
+
+                name = Prompt.ask("[input]Enter name of the book[/]")
+                author = Prompt.ask("[input]Enter name of the author[/]")
+                genre = Prompt.ask("[input]Enter book genres (seperated by space)[/]")
+
+                print("\n\n")
+
+                id, name = self.db.book_new(name, author, genre)
+                self.console.print(
+                    f"[green b]Your book [magenta]{name}[/] has been added to the database with the BookID of [magenta]{id}[/].",
+                    "[openquit]Press enter to proceed..."
+                )
+
+                input()
+
+                self.path.pop()
+                continue
 
 
 if __name__ == "__main__":
